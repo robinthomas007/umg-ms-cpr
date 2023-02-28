67acfae1cf4849dbf5464c3f289417d8c2bc71e1
ramand <dineshkumar.raman@umusic.com>
2023-02-28 23:29:51 +0530

    Remove unnecessary timeout for logout

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import getAuthUser from './utils/getAuthUser'
 import Unauthorized from './globalComponents/Unauthorized'
-import { useEffect } from 'react'
 import Pages from './pages'
 import LayOut from './globalComponents/LayOut'
 
 function App() {
   const user = getAuthUser()
-
-  useEffect(() => {
-    if (user) {
-      setTimeout(() => {
-        alert('Session timed out!')
-        window.location.reload()
-      }, user.exp)
-    }
-  }, [user])
 
   if (!user) {
     return <Unauthorized />
