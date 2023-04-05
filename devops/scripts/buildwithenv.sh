6ad39b9cfc7b6abcd999b99282335e7b35122c53
Robin Thomas <Robin.Thomas@umusic.com>
2023-04-05 11:52:10 +0530

    deops final

--- devops/scripts/buildwithenv.sh
+++ devops/scripts/buildwithenv.sh
@@ -2,7 +2,9 @@
 
 
 USER_DATA=`echo "$USER_DATA" | base64 --decode`
+ENV_VARIABLE=`echo "$USER_DATA" | jq -r '.ENV_VARIABLE'`
 
 echo "userdata ------------------------------======================= $USER_DATA"
+echo "ENV_VARIABLE ------------------------------======================= $ENV_VARIABLE"
 
-npm run build:dev
+npm run build:$ENV_VARIABLE
