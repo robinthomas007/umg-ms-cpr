c9619b95f5a4fd0da3d2c4d9874f8de006b4403e
Vinoth Periyasamy <vinothperiyasamy@AGLS-MLT-579.local>
2023-09-13 14:11:15 +0530

    modified the env urls config

--- src/config.tsx
+++ src/config.tsx
@@ -1,6 +1,6 @@
 export function cprUrls(env: string) {
-  const cpr_app_urls = {
-    url: 'https://api.dev.cpr.umgapps.com/api/',
+  const cpr_app_urls: any = {
+    url: 'https://api.dev.cpr-portal.umgapps.com/gateway/cpr/',
   }
   // eslint-disable-next-line default-case
   switch (env) {
@@ -11,12 +11,12 @@ export function cprUrls(env: string) {
       cpr_app_urls.url = 'https://api.dev.cpr-portal.umgapps.com/gateway/cpr/'
       return cpr_app_urls
     case 'uat':
-      cpr_app_urls.url = 'https://api.stage.cpr-portal.umgapps.com/gateway/cpr'
+      cpr_app_urls.url = 'https://api.stage.cpr-portal.umgapps.com/gateway/cpr/'
       return cpr_app_urls
     case 'prod':
-      cpr_app_urls.url = 'https://api.cpr-portal.umgapps.com/gateway/cpr'
+      cpr_app_urls.url = 'https://api.cpr-portal.umgapps.com/gateway/cpr/'
       return cpr_app_urls
     default:
-      return 'https://api.dev.cpr-portal.umgapps.com/gateway/cpr'
+      return 'https://api.dev.cpr-portal.umgapps.com/gateway/cpr/'
   }
 }
